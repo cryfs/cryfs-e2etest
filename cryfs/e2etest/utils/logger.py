b4288b70ca831b2ee005c2ed97a3bbac88511d5d
Sebastian Messmer <messmer@cryfs.org>
2018-04-15 18:09:46 -0700

    Add simple test logger

---
+++ cryfs/e2etest/utils/logger.py
@@ -0,0 +1,37 @@
+from typing import List
+import attr
+from functools import reduce
+from enum import Enum
+
+
+class LogLevel(Enum):
+    INFO = 1
+    WARNING = 2
+    ERROR = 3
+
+    def to_string(self) -> str:
+        return {
+            LogLevel.INFO: "INFO",
+            LogLevel.WARNING: "WARNING",
+            LogLevel.ERROR: "ERROR",
+        }[self]
+
+
+@attr.s(auto_attribs=True)
+class LogEntry(object):
+    level: LogLevel = LogLevel.INFO
+    message: str = ""
+
+    def to_string(self) -> str:
+        return "[%s] %s" % (self.level.to_string(), self.message)
+
+
+class Logger(object):
+    def __init__(self) -> None:
+        self._log: List[LogEntry] = []
+
+    def log(self, level: LogLevel, message: str) -> None:
+        self._log.append(LogEntry(level=level, message=message))
+
+    def to_string(self) -> str:
+        return reduce(lambda s, i: s + i.to_string() + "\n", self._log, "")
---
+++ tests/cryfs/e2etest/utils/logger_test.py
@@ -0,0 +1,7 @@
+from cryfs.e2etest.utils.logger import Logger, LogLevel
+
+def test_logger() -> None:
+    logger = Logger()
+    logger.log(LogLevel.INFO, "Message 1")
+    logger.log(LogLevel.ERROR, "Message 2")
+    assert logger.to_string() == "[INFO] Message 1\n[ERROR] Message 2\n"
