22a5d2e4fe7883eb713c1d0a4e7749a657819ab1
Sebastian Messmer <mail@smessmer.de>
2018-04-18 19:54:14 -0700

    Added constructed_cryfs0.9.9 compatibility test

--- cryfs/e2etest/compatibility_test.py
+++ cryfs/e2etest/compatibility_test.py
@@ -62,6 +62,10 @@ fixtures = [Fixture(
     password=b"mypassword",
 ), Fixture(
     data="fixtures/constructed_data.tar",
+    encoded="fixtures/constructed_cryfs0.9.9_encoded.tar",
+    password=b"mypassword",
+), Fixture(
+    data="fixtures/constructed_data.tar",
     encoded="fixtures/constructed_cryfs0.10-m2+194.gb0077e7a_encoded.tar",
     password=b"mypassword",
 )]
